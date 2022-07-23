14e23283f950b28804d1cc2f88926b28682bf8a4
ravipogaku <109363841+ravipogaku@users.noreply.github.com>
2022-07-23 17:21:59 +0530

    Update WebappEx.py

--- WebappEx.py
+++ WebappEx.py
@@ -20,12 +20,13 @@ st.header('User Input Parameters')
 def user_input_features():
     firstNumber = st.number_input("First_Number",min_value=1,max_value=400000 ,step=1)
     secondNumber = st.number_input("Second_Number",min_value=1,max_value=400000 ,step=1)
-    divisionResult = st.number_output("Result")
+    thirdNumber = st.number_input("Quotient_Number")
+    
     
     data = {
             'First_Number': firstNumber,
             'Second_Number': secondNumber,
-            'Result': divisionResult
+            'Quotient_Number': thirdNumber
             
             }
     features = pd.DataFrame(data, index=[0])
@@ -35,4 +36,10 @@ df = user_input_features()
 
 st.subheader('User Input parameters')
 st.write(df.to_dict())
+
+df['Quotient_Number'] = df['First_Number']/df['Second_Number']
+
+st.write("Result",df['Quotient_Number'])
+
+
     
