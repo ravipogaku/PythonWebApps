bda657c114585594f776712eaac850f0bc6ff99d
ravipogaku <109363841+ravipogaku@users.noreply.github.com>
2022-07-23 17:06:43 +0530

    Update WebappEx.py

--- WebappEx.py
+++ WebappEx.py
@@ -9,9 +9,9 @@ import streamlit as st
 import pandas as pd
 
 st.write("""
-# Credit Card Approval Prediction App
+# Division App
 
-This app predicts the credit card approval probablity
+This app divides first_number by second_number
 """)
 #Get Input
 
@@ -20,10 +20,12 @@ st.header('User Input Parameters')
 def user_input_features():
     firstNumber = st.number_input("First_Number",min_value=1,max_value=400000 ,step=1)
     secondNumber = st.number_input("Second_Number",min_value=1,max_value=400000 ,step=1)
+    result = st.number_output("Result")
     
     data = {
             'First_Number': firstNumber,
-            'Second_Number': secondNumber
+            'Second_Number': secondNumber,
+            'Result': result
             
             }
     features = pd.DataFrame(data, index=[0])
