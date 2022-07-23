e3019be99967244221a4d6eb83defe44202e5246
ravipogaku <109363841+ravipogaku@users.noreply.github.com>
2022-07-23 16:37:42 +0530

    Create WebappEx.py

---
+++ WebappEx.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul 23 16:32:04 2022
+
+@author: ravip
+"""
+
+import streamlit as st
+import pandas as pd
+
+st.write("""
+# Credit Card Approval Prediction App
+
+This app predicts the credit card approval probablity
+""")
+#Get Input
+
+st.header('User Input Parameters')
