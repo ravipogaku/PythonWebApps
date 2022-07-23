6e5b84b5047f07affe8d7474b2e4837c002b92db
ravipogaku <109363841+ravipogaku@users.noreply.github.com>
2022-07-23 17:28:31 +0530

    Update WebappEx.py

--- WebappEx.py
+++ WebappEx.py
@@ -20,13 +20,13 @@ st.header('User Input Parameters')
 def user_input_features():
     firstNumber = st.number_input("First_Number",min_value=1,max_value=400000 ,step=1)
     secondNumber = st.number_input("Second_Number",min_value=1,max_value=400000 ,step=1)
-    thirdNumber = st.number_input("Quotient_Number")
+    thirdNumber = st.number_input("Quotient")
     
     
     data = {
             'First_Number': firstNumber,
             'Second_Number': secondNumber,
-            'Quotient_Number': thirdNumber
+            'Quotient': thirdNumber
             
             }
     features = pd.DataFrame(data, index=[0])
@@ -37,10 +37,10 @@ df = user_input_features()
 st.subheader('User Input parameters')
 st.write(df.to_dict())
 
-df['Quotient_Number'] = df['First_Number']/df['Second_Number']
-df['Quotient_Number'] = df['Quotient_Number'].replace({1:1,0:0})
+df['Quotient'] = df['First_Number']/df['Second_Number']
+df['Quotient'] = df['Quotient'].replace(df['Quotient'])
 
-st.write("Result",df['Quotient_Number'])
+st.write("Result",df['Quotient'])
 
 
     
